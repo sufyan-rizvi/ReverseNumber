f96f4445603ff7716f8fcbb396fba2c144883d4e
sufyan-rizvi <sofylaiq@gmail.com>
2024-07-16 21:05:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber;
+
+            Console.WriteLine("Input the number to be reversed: ");
+            myNumber = Convert.ToInt32(Console.ReadLine());
+
+            int reversed = 0;
+            int digit = 0;
+
+            while (myNumber > 0)
+            {
+                digit = myNumber % 10;
+                reversed = (reversed * 10) + digit;
+                myNumber = myNumber /10 ;
+                
+            }
+            Console.WriteLine(reversed);
+        }
+
+    }
+}
+
